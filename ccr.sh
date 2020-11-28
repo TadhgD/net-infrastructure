eae89995094462092cef17ba2b1519b7a35d72b0
tdowney <tdowney@firstderivatives.com>
2020-11-28 11:59:01 -0500

    Improved logging in ccr.sh script, tidied up output

--- ccr.sh
+++ ccr.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
 aws cloudformation describe-stacks \
-	--stack-name $1
+	--stack-name $1 >/dev/null 2>&1
 
 if [[ $? -eq 255 ]]
 then
+	echo "Stack '${1}' not found. Creating stack..."
 	aws cloudformation create-stack \
 	        --stack-name $1 \
 	        --template-body file://$2 \
 	        --parameters file://$3 \
 	        --region=us-west-2
 else
+	echo "Stack '${1}' is online. Updating stack..."
 	aws cloudformation update-stack \
 	        --stack-name $1 \
 	        --template-body file://$2 \
